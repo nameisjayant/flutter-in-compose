c740ed66c898124586d66be3210b521ed0c3c739
nameisjayant <jayntkumar99@gmail.com>
2023-01-27 10:49:56 +0530

    init project

---
+++ flutter-in-compose/src/main/java/com/nameisjayant/flutter_in_compose/sample.kt
@@ -0,0 +1,13 @@
+package com.nameisjayant.flutter_in_compose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SampleDemo() {
+
+    Box{
+
+    }
+}
